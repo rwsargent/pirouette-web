6c938a1fd05dfd5423e4c369d7d4d95d738678ef
rwsargent <rw.sarge@gmail.com>
2016-06-17 16:20:31 -0600

    lookin' like sm now

--- app/static/public/js/leftright.js
+++ app/static/public/js/leftright.js
@@ -12,6 +12,10 @@ function stop() {
     socket.emit('stop', "");
 }
 
+function onkeydown(event) {
+    
+}
+
 function setupSockets() {
     var namespace = '/pir'; // change to an empty string to use the global namespace
     // socket = new WebSocket("ws://" + document.domain + ":" + location.port + "/pir");
@@ -26,4 +30,30 @@ function setupSockets() {
 
 $(document).ready(function() {
     setupSockets();
-})
+    var $left = $(".circle.left");
+    var $right = $(".circle.right");
+    
+    window.addEventListener('keydown', function(event) {
+        var code = event.keyCode;
+        if(code === 37) {
+            left();
+//            $left.addClass("hovered");
+            $left.addClass("pulsate");
+        } else if (code === 39) {
+            right();
+//            $right.addClass("hovered");
+            $right.addClass("pulsate");
+        }
+    }, false);
+
+    window.addEventListener('keyup', function(event) {
+        var code = event.keyCode;
+        if(code === 37) {
+            stop();
+        } else if (code === 39) {
+            stop();
+        }
+        $left.removeClass("pulsate");
+        $right.removeClass("pulsate");
+    }, false);
+});
